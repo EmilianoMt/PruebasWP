376d58c9def455caad3ee6cf557365a21ce22b44
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2025-08-19 14:22:19 -0600

    some fixes

--- scripts/export_db.sh
+++ scripts/export_db.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-docker-compose exec wordpress wp db export /var/www/html/db_export.sql
+
+# Crear directorio para dumps si no existe
+mkdir -p ./db_dump
+
+# Exportar la base de datos
+docker-compose exec -T wordpress wp db export /var/www/html/db_export.sql
+
+# Copiar el archivo exportado al host
 docker cp $(docker-compose ps -q wordpress):/var/www/html/db_export.sql ./db_dump/db_export.sql
-run
+
+# Limpiar el archivo temporal del contenedor
+docker-compose exec -T wordpress rm /var/www/html/db_export.sql
--- scripts/import_db.sh
+++ scripts/import_db.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+# Verificar si existe el archivo a importar
+if [ ! -f "./db_dump/db_export.sql" ]; then
+    echo "Error: No se encuentra el archivo de base de datos en ./db_dump/db_export.sql"
+    exit 1
+fi
+
+# Copiar el archivo al contenedor
 docker cp ./db_dump/db_export.sql $(docker-compose ps -q wordpress):/var/www/html/db_import.sql
-docker-compose exec wordpress wp db import /var/www/html/db_import.sql
-run
+
+# Importar la base de datos
+docker-compose exec -T wordpress wp db import /var/www/html/db_import.sql
+
+# Limpiar el archivo temporal del contenedor
+docker-compose exec -T wordpress rm /var/www/html/db_import.sql
+
+echo "Base de datos importada exitosamente"
